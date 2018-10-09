0aac521a16dbf4592a740ba1fb61cdba9a53f857
vilko <helgi8@mail.ru>
2018-10-09 04:21:59 +0300

    Добавьте файлы проекта.

---
+++ Type_conversion_errors/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// tenThousandPi десять тысяч пи
+// roundedTenThousandPi округленная десятка тысяч пи
+// integerPartOfTenThousandPi целое число из десяти тысяч пи
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_conversion_errors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            double tenThousand = 10000L;
+            float tenThousandPi = (float)(pi * tenThousand);
+            int roundedTenThousandPi = (int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)(tenThousandPi);
+            Console.WriteLine(integerPartOfTenThousandPi);
+            Console.WriteLine(roundedTenThousandPi);
+            Console.ReadKey();
+        }
+    }
+}
